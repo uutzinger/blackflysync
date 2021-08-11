823a8ca30234b091ff739632375e3911cc297e09
uutzinger <utzinger@email.arizona.edu>
2021-08-11 09:37:42 -0700

    create help request

--- digitalWriteISR/digitalWriteISR.ino
+++ digitalWriteISR/digitalWriteISR.ino
@@ -7,19 +7,19 @@
 // *********************************************************************************************//
 // Pin Settings
 // ------------------------------------------------------------------------
-#define NUM_CHANNELS       3 // Number of LED channels
-#define CH1                2 // Connect pin 2 to LED driver channel 1 (output)
-#define CH2                3 // Connect pin 3 to LED driver channel 2 (output)
-#define CH_BG             -1 // LED driver background channel, all LEDs off  
-#define CLK               22 // Connect pin 22 to LED driver PWM CLK (output)
-#define CAMTRG            21 // Connect pin 21 to Camera frame trigger (input)
+#define NUM_CHANNELS       3 // Number of channels
+#define CH1                2 // Connect pin 2 to channel 1 (output)
+#define CH2                3 // Connect pin 3 to channel 2 (output)
+#define CH_BG             -1 // Connect no pin to channel 3  
+#define CLK               22 // Connect pin 22 to CLK (output)
+#define TRG               21 // Connect pin 21 to trigger (input)
 #define LEDPIN            13 // Built in LED
 #define TURN_ON  HIGH        // Define Enable Channel
 #define TURN_OFF LOW         // Define Disable Channel
 
 // PWM Settings
 // ------------------------------------------------------------------------
-#define PWM_Frequency  50000
+#define PWM_Frequency      5
 #define PWM_Resolution     8   
 #define PWM_MaxValue     255
 #define DutyCycle        5.0  
@@ -28,7 +28,7 @@
 // ------------------------------------------------------------------------
 volatile int LEDs[NUM_CHANNELS] = {CH1, CH2, CH_BG }; // LED channel array
 volatile int currentChannel = 0;                      // Index to current LED in LED array
-volatile bool frameTriggerOccured = false;            // Signal to main loop
+volatile bool triggerOccured = false;                 // Signal to main loop
 bool ledStatus = false;                               // Blinking of the built in LED
 
 // *********************************************************************************************//
@@ -43,15 +43,15 @@ void setup(){
   pinMode(LEDPIN,       OUTPUT); digitalWrite(LEDPIN,LOW ); 
 
   // Configure Input Pins
-  pinMode(CAMTRG, INPUT_PULLUP);  // Frame trigger
+  pinMode(TRG, INPUT_PULLUP);  // trigger
 
   // Configure Interrupts
-  attachInterrupt(digitalPinToInterrupt(CAMTRG), frameISR, RISING);            // ISR occurs at frame exposure start
+  attachInterrupt(digitalPinToInterrupt(TRG), myISR, RISING);
  
   // Configure PWM output
-  //analogWriteResolution(PWM_Resolution);                                       // change resolution
-  //analogWriteFrequency(CLK, PWM_Frequency);                                    // set PWM frequency on CLK pin
-  //analogWrite(CLK, uint16_t((100.0-DutyCycle) / 100.0 * float(PWM_MaxValue))); // set Duty Cyle and enable PWM on CLK pin (i have inverted PWM logic)
+  analogWriteResolution(PWM_Resolution);                               // change resolution
+  analogWriteFrequency(CLK, PWM_Frequency);                            // set PWM frequency on CLK pin
+  analogWrite(CLK, uint16_t(DutyCycle / 100.0 * float(PWM_MaxValue))); // set Duty Cyle and enable PWM on CLK pin (i have inverted PWM logic)
 
   // Start Serial IO
   Serial.begin(BAUDRATE);
@@ -66,10 +66,10 @@ void loop(){
     
   // Blink LED if ISR occurs
   // ------------------------------------------------------------------------
-  if (frameTriggerOccured) {
+  if (triggerOccured) {
     ledStatus = !ledStatus;
     digitalWriteFast(LEDPIN, ledStatus); // blink
-    frameTriggerOccured = false;         // reset signal
+    triggerOccured = false;              // reset signal
     Serial.println(currentChannel);      // debug
   }
 
@@ -79,13 +79,13 @@ void loop(){
 // Support Functions                                                                            //
 // *********************************************************************************************//
 
-void frameISR() {
-  // Turn OFF previous LED, skip the background channel
+void myISR() {
+  // Turn OFF previous channel, skip the background channel
   if (LEDs[currentChannel] != CH_BG) { digitalWriteFast(LEDs[currentChannel], TURN_OFF); } 
   // Increment channel index
   currentChannel = currentChannel + 1;
   if ( currentChannel == NUM_CHANNELS ) { currentChannel = 0; }
   // Turn ON next LED, skip the background channel
   if (LEDs[currentChannel] != CH_BG) { digitalWriteFast(LEDs[currentChannel], TURN_ON); } 
-  frameTriggerOccured = true;  // signal to main loop
+  triggerOccured = true;  // signal to main loop
 }
